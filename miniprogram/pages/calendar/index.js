3f2d5a858c96d08410f30208d4d608569029add9
luowenshuai <542270798@qq.com>
2021-03-16 19:14:34 +0800

    优化日历管理

--- miniprogram/pages/calendar/index.js
+++ miniprogram/pages/calendar/index.js
@@ -1,5 +1,11 @@
 //index.js
-const app = getApp()
+const app = getApp();
+const nowDate = new Date();
+const [nowYear, nowMonth, nowDay] = [
+  nowDate.getFullYear(),
+  nowDate.getMonth(),
+  nowDate.getDate(),
+];
 const weekMapList = [
   {
     weekNumber: 1,
@@ -38,68 +44,36 @@ const switchDateObject = date => {
     : new Date(date.replace(/-/g, '/'));
 };
 
-// 获取某月天数
-// date 格式 ‘Date 对象’ 或 ‘2020-9’
-const getMonthDays = (date = new Date()) => {
-  const targetDate = switchDateObject(date);
-  // 获取当前月份
-  const curMonth = targetDate.getMonth();
-  // 设置当前月1日，防止下一个月没有29-31日
-  targetDate.setDate(0);
-  // 将 targetDate 设置为下一个月
-  targetDate.setMonth(curMonth + 1);
-  // setDate(0) 会让 targetDate 日期变成前一个月的最后一天，也就是当前月的最后一天
-  targetDate.setDate(0);
-  // 获取 targetDate 的日期，也就是当前月的最后一天的日期，即当前月天数
-  return targetDate.getDate();
-};
-
-// 是否是当年当月当日
-const isCurYearMonthDay = (date, dayNumber) => {
-  const targetDate = switchDateObject(date);
-  const nowDate = new Date();
-  const curDay = nowDate.getDate(); // 当天日期
-  return (
-    targetDate.getFullYear() === nowDate.getFullYear() &&
-    targetDate.getMonth() === nowDate.getMonth() &&
-    dayNumber === curDay
-  ); // 是否为当年当月
-};
-
 // 获取初始列表，6 横 7 列
 // date 格式 Date 对象 或 '2020-9'
-const getInitList = (date = new Date()) => {
+const getInitList = (date = nowDate) => {
   const targetDate = switchDateObject(date);
   targetDate.setDate(1); // 设置为当月第一天的星期数
   const targetWeekNumber = targetDate.getDay() || 7; // 目标月第一天
-  const targetMonthDays = getMonthDays(targetDate); // 目标月天数
-  targetDate.setDate(0); // 设置为上一月最后一天
-  const lastMonthDays = getMonthDays(targetDate); // 上个月天数
 
   const initList = [];
   for (let i = 0; i < 6; i += 1) {
     const listItem = weekMapList.map((item, index) => {
       let dayNumber = i * 7 + index - targetWeekNumber + 2; // 0
-      let isTargetMonth = false;
-      let isCurDay = false;
-      switch (true) {
-        case dayNumber < 1:
-          dayNumber += lastMonthDays;
-          break;
-        case dayNumber > targetMonthDays:
-          dayNumber -= targetMonthDays;
-          break;
-        default:
-          isTargetMonth = true;
-          isCurDay = isCurYearMonthDay(date, dayNumber);
-          break;
-      }
+      const dateObj = switchDateObject(date);
+      dateObj.setDate(dayNumber);
+      const [itYear, itMonth, itDay] = [
+        dateObj.getFullYear(),
+        dateObj.getMonth(),
+        dateObj.getDate(),
+      ];
 
+      // 目标月
+      const [targetYear, targetMonth] = [
+        targetDate.getFullYear(),
+        targetDate.getMonth(),
+      ];
       return {
         ...item,
-        dayNumber,
-        isTargetMonth,
-        isCurDay,
+        dateObj,
+        isTargetMonth: itYear === targetYear && itMonth === targetMonth,
+        isCurDay: itYear === nowYear && itMonth === nowMonth && itDay === nowDay,
+        dayNumber: itDay,
       };
     });
     initList.push({
@@ -119,6 +93,7 @@ const getYearMonth = date => {
 };
 Page({
   data: {
+    weekMapList,
     targetDate: undefined,
     dateTitle: '',
     calendarList: [],
